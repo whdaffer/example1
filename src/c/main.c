f224a102683769be4e3d75ba5037cac72fa016f4
George Daffer <ghd@gmail.com>
2023-02-24 16:06:16 -0800

    Again, it asks for a change message

--- src/c/main.c
+++ src/c/main.c
@@ -1,7 +1,11 @@
-/* Time-stamp: <2023-02-17 16:22:02 PST> */
+/* Time-stamp: <2023-02-24 15:54:19 ghd> */
 /* silly comment */
 #include <stdio.h>
 int main(){
   printf("Hello world from newly cloned example1!\n");
+  int i;
+  for (i=0;i++;i<10){
+      printf("i = %d\n",i);
+  }
   return 0;
 };
